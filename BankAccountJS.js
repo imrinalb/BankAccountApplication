f35b4308a1283fd1f07fe463095e655a493fbf62
imrinalb <63386758+imrinalb@users.noreply.github.com>
2022-05-03 15:23:43 -0500

    Add files via upload

--- BankAccountJS.js
+++ BankAccountJS.js
@@ -1,3 +1,40 @@
+(()=>{
+    let accountInfoList = [];
+
+let DOM = (function IIFE(){
+   return {
+        name: document.getElementById("name"),
+        deposit: document.getElementById("deposit"),
+        submit: document.getElementById("submit"),
+        textArea: document.getElementById("textArea")
+    };
+})();
+
+let createAcount = (name, deposit) => {
+    return {
+        name: name,
+        deposit: deposit
+    }
+};
+
+let btnHandler = () =>{
+        if(DOM.name.value != "" && DOM.deposit.value != ""){
+            accountInfoList.push(createAcount(DOM.name.value, DOM.deposit.value));
+            DOM.textArea.innerHTML = "";
+            accountInfoList.forEach(element => {
+        
+                DOM.textArea.innerHTML += "Account Name: " + element.name + " Balance : " + element.deposit + "\n";
+                DOM.name.value = "";
+                DOM.deposit.value = "";
+            });
+        }
+       
+    };
+
+DOM.submit.addEventListener("click", btnHandler);
+})();
+
+
 
 function create(){
     var inputName= document.getElementById("accName");
@@ -38,33 +75,3 @@ function create(){
     document.getElementById("demo2").innerHTML=output.getNewBanking();
     document.getElementById("demo3").innerHTML=output. getDebit();
 }
-
-
-// function newDeposit(){
-//     var inputName= document.getElementById("accName");
-//     var inputDeposit= document.getElementById("deposit");
-//     var inputNewDeposit= document.getElementById("newDeposit");
-
-//     class Banking{
-//         #name;
-//         #depo;
-//         #newDepo;
-
-//         constructor(){
-//             this.#name= inputName.value;
-//             this.#depo= inputDeposit.value;
-//             this.#newDepo= inputNewDeposit.value;
-//         }
-
-
-//         getBanking(){
-//             if(this.#newDepo.value>0){
-//                 this.#depo.value += this.#newDepo.value;
-//             }
-//             return "Account Name: " +this.#name +", Balance: " +this.#depo;
-//         }
-//     }
-
-//     const output=  new Banking();
-//     document.getElementById("demo").innerHTML=output.getBanking();
-// }
